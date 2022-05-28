ca4935fc3ee4deb2272b7a64909631dd01b74fd5
Kefa Vova <kefavova24@gmail.com>
2022-05-28 11:13:22 +0300

    add ripple effect on button

--- src/main/java/me/chat/controller/LoginController.java
+++ src/main/java/me/chat/controller/LoginController.java
@@ -1,17 +1,31 @@
 package me.chat.controller;
 
+import com.jfoenix.controls.JFXRippler;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
 import me.chat.ChatClientMain;
 import me.chat.event.PlayerJoinEvent;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
 
     @FXML
     private TextField textbox;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Pane pane;
 
     @FXML
     protected void enterPressedEvent(KeyEvent event) {
@@ -32,5 +46,15 @@ public class LoginController {
     @FXML
     protected void onExit() {
         Platform.exit();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Добавляємо Ripple ефект на кнопку
+        JFXRippler jfxRippler = new JFXRippler(this.button);
+        jfxRippler.setRipplerFill(Paint.valueOf("#FDEBAE"));
+        jfxRippler.setLayoutX(99);
+        jfxRippler.setLayoutY(228);
+        pane.getChildren().add(jfxRippler);
     }
 }
